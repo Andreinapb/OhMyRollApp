c4bfb76e8f23eb925bc4a7b4c24866673b786902
Andreina Parra <andreinamariap@gmail.com>
2021-06-09 01:08:09 -0300

    boton

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,5 +1,9 @@
 
 import React, {useState}from 'react'
+import './ItemCount.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons'; 
+import {faMinusCircle} from '@fortawesome/free-solid-svg-icons'
 
 const ItemCount = () => {
 
@@ -7,18 +11,23 @@ const ItemCount = () => {
     const Initial = 1
     const [Contador, setContador] = useState (Initial)
 
+    const onAdd = (producto) => {
+    alert (`compraste ${producto} productos`)
+    }
+
     return (
         <div>
 
-<button
-onClick = { () => Contador > 0 ? setContador (Contador - 1) : alert ("Por favor Agrega un producto")}>-</button> 
+<div className='caja-contador'> 
+<div>
+<FontAwesomeIcon icon={faMinusCircle} className='icon' onClick = { () => Contador > 0 ? setContador (Contador - 1) : alert ("Por favor Agrega un producto")}></FontAwesomeIcon>
+<span className='contador'>{Contador}</span>
+<FontAwesomeIcon icon={faPlusCircle} className='icon' button onClick = { () => Contador < Stock ? setContador (Contador + 1) : alert ("Se nos ha acabado el stock, por favor vuelve mañana")}></FontAwesomeIcon>
+</div>
 
-<span>{Contador}</span>
-
-<button onClick = { () => Contador < Stock ? setContador (Contador + 1) : alert ("Se nos ha acabado el stock, por favor vuelve mañana")}>+</button>
-
-<button onClick = { () => alert ("fue agreado a su carrito") }> Agregar Productos</button>
-
+<button onClick = { () => onAdd (Contador) } className='boton' type='button'> Agregar Productos</button>
+</div>
+       
         </div>
     )
 }
